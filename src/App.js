7f5c949c3e9f1e1b552124e170b650dcba952b58
Nathan Mundo <nathanmundo@icloud.com>
2024-07-08 15:26:07 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Grid,
+  Container,
+  TextField,
+  Typography,
+  Button,
+  Card,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+} from "@mui/material";
+import { Fragment, useEffect, useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = () => {
+    if (todo) {
+      const newTodo = {
+        id: crypto.randomUUID(),
+        text: todo,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+      setTodo("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h3">To Do List</Typography>
+        </Grid>
+        <Grid item xs={12} md={10}>
+          <TextField
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            size="small"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} md={2}>
+          <Button variant="contained" size="medium" onClick={addTodo} fullWidth>
+            Add
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <List>
+              {todos.map(({ id, text }, index) => (
+                <Fragment key={id}>
+                  {index !== 0 && <Divider />}
+                  <ListItem>
+                    <ListItemText>{text}</ListItemText>
+                  </ListItem>
+                </Fragment>
+              ))}
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
